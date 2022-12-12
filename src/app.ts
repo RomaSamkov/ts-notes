daa4349aa078e0db2a330552b2bea2844fc3c123
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-12 22:06:02 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 class House {
-  street: string;
+  public street: string;
+  public tenants: string[] = [];
   constructor(n: string) {
     this.street = n;
   }
-  showAdress(): void {
-    console.log('Adress:' + this.street);
+  public showAdress(this: House, add: string): void {
+    console.log('Adress:' + this.street + ' ' + add);
+  }
+  public addTenant(name: string) {
+    this.tenants.push(name);
+  }
+  public showTenant() {
+    console.log(this.tenants);
   }
 }
 
 const house = new House('Street');
-house.showAdress();
