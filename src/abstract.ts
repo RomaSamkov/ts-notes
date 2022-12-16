a40d8836af9e70d6b7a5c02fe70510ac9695aa2b
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-16 23:22:33 +0200

    Create abstract.ts

---
+++ src/abstract.ts
@@ -0,0 +1,29 @@
+abstract class Plane {
+  protected pilot = false;
+
+  public sitInPlane() {
+    this.pilot = true;
+  }
+
+  public abstract startEngine(): string;
+}
+
+class Maize extends Plane {
+  public startEngine() {
+    return 'br-br-br';
+  }
+}
+class Boing extends Plane {
+  public startEngine() {
+    return 'b-b-b';
+  }
+}
+
+const maize = new Maize();
+const boing = new Boing();
+
+maize.sitInPlane();
+boing.sitInPlane();
+
+console.log(maize.startEngine());
+console.log(boing.startEngine());
