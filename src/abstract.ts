38f6e345f9d17ea8ad6614c343b9f7ccdd0920ce
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-17 23:16:18 +0200

    Update abstract.ts

--- src/abstract.ts
+++ src/abstract.ts
@@ -1,29 +1,29 @@
-abstract class Plane {
-  protected pilot = false;
+// abstract class Plane {
+//   protected pilot = false;
 
-  public sitInPlane() {
-    this.pilot = true;
-  }
+//   public sitInPlane() {
+//     this.pilot = true;
+//   }
 
-  public abstract startEngine(): string;
-}
+//   public abstract startEngine(): string;
+// }
 
-class Maize extends Plane {
-  public startEngine() {
-    return 'br-br-br';
-  }
-}
-class Boing extends Plane {
-  public startEngine() {
-    return 'b-b-b';
-  }
-}
+// class Maize extends Plane {
+//   public startEngine() {
+//     return 'br-br-br';
+//   }
+// }
+// class Boing extends Plane {
+//   public startEngine() {
+//     return 'b-b-b';
+//   }
+// }
 
-const maize = new Maize();
-const boing = new Boing();
+// const maize = new Maize();
+// const boing = new Boing();
 
-maize.sitInPlane();
-boing.sitInPlane();
+// maize.sitInPlane();
+// boing.sitInPlane();
 
-console.log(maize.startEngine());
-console.log(boing.startEngine());
+// console.log(maize.startEngine());
+// console.log(boing.startEngine());
