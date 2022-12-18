9982afe307016f525f6995d187d415c9229959e5
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-18 23:10:52 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,39 @@
-class House {
-  private tenants: string[] = [];
+// class House {
+//   private tenants: string[] = [];
 
-  constructor(public type: string, protected street: string) {}
+//   constructor(public type: string, protected street: string) {}
 
-  public showAdress(this: House): void {
-    console.log('Adress:' + this.street);
-  }
-  public showType(this: House): void {
-    console.log('Type:' + this.type);
-  }
-  public addTenant(name: string) {
-    this.tenants.push(name);
-  }
-  public showTenants() {
-    console.log(this.tenants);
-  }
-}
+//   public showAdress(this: House): void {
+//     console.log('Adress:' + this.street);
+//   }
+//   public showType(this: House): void {
+//     console.log('Type:' + this.type);
+//   }
+//   public addTenant(name: string) {
+//     this.tenants.push(name);
+//   }
+//   public showTenants() {
+//     console.log(this.tenants);
+//   }
+// }
 
-class StoneHouse extends House {
-  private chatHouse: string;
-  constructor(street: string, main: string) {
-    super('stone', street);
-    this.chatHouse = main;
-  }
-  public showTenants() {
-    console.log('Main:' + this.chatHouse);
-    super.showTenants();
-  }
-  public showAdress(): void {
-    console.log('Adress:' + this.street);
-  }
-}
+// class StoneHouse extends House {
+//   private chatHouse: string;
+//   constructor(street: string, main: string) {
+//     super('stone', street);
+//     this.chatHouse = main;
+//   }
+//   public showTenants() {
+//     console.log('Main:' + this.chatHouse);
+//     super.showTenants();
+//   }
+//   public showAdress(): void {
+//     console.log('Adress:' + this.street);
+//   }
+// }
 
-const house = new StoneHouse('StoneStreet', 'User');
-house.addTenant('Sexy');
-house.addTenant('Pretty');
-house.showTenants();
-house.showAdress();
+// const house = new StoneHouse('StoneStreet', 'User');
+// house.addTenant('Sexy');
+// house.addTenant('Pretty');
+// house.showTenants();
+// house.showAdress();
