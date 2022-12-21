8f132df518f4bc2af8822f74c9a3ddcfc9d23018
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-21 23:14:15 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,11 @@
-// class House {
-//   private tenants: string[] = [];
+type Admin = {
+  name: string;
+  previligious: string[];
+};
 
-//   constructor(public type: string, protected street: string) {}
+type User = {
+  name: string;
+  startDate: Date;
+};
 
-//   public showAdress(this: House): void {
-//     console.log('Adress:' + this.street);
-//   }
-//   public showType(this: House): void {
-//     console.log('Type:' + this.type);
-//   }
-//   public addTenant(name: string) {
-//     this.tenants.push(name);
-//   }
-//   public showTenants() {
-//     console.log(this.tenants);
-//   }
-// }
-
-// class StoneHouse extends House {
-//   private chatHouse: string;
-//   constructor(street: string, main: string) {
-//     super('stone', street);
-//     this.chatHouse = main;
-//   }
-//   public showTenants() {
-//     console.log('Main:' + this.chatHouse);
-//     super.showTenants();
-//   }
-//   public showAdress(): void {
-//     console.log('Adress:' + this.street);
-//   }
-// }
-
-// const house = new StoneHouse('StoneStreet', 'User');
-// house.addTenant('Sexy');
-// house.addTenant('Pretty');
-// house.showTenants();
-// house.showAdress();
+type AdminUser = Admin & User;
