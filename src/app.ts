b427879529182659efedcd0d47124eadc0741d5c
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-28 23:04:08 +0200

    add decorator methods

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,11 @@
-/*
-function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
-  const method = descriptor.value as Function;
-
-  return {
-    configurable: true,
-    enumerable: false,
-    get() {
-      return method.bind(this);
-    },
-  };
-}
-*/
-
-function AddTax(tax: number) {
-  return function (_: any, _2: string, descriptor: PropertyDescriptor) {
-    const method = descriptor.value as Function;
-
-    return {
-      configurable: true,
-      enumerable: false,
-      get() {
-        return (...args: any[]) => {
-          const result = method.apply(this, args);
-          return result + (result / 100) * tax;
-        };
-      },
-    };
-  };
+function CheckEmail(target: any, name: string, position: number) {
+  console.log('target:', target);
+  console.log('name:', name);
+  console.log('position:', position);
 }
 
-class Payment {
-  @AddTax(20)
-  pay(money: number) {
-    return money;
+class Person {
+  setEmail(_: number, @CheckEmail email: string) {
+    console.log(email);
   }
 }
-
-const payment = new Payment();
-
-console.log(payment.pay(100));
