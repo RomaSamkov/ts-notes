ee6bb262932c6c8a7b9014a09e06de70b65ca2f5
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-02 19:16:29 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 const happyHoliday = 'Happy New Year!!!';
 const happyHewYear = 'Happy New Year !!!';
 const happyDeveloper = 'Happy Developer.';
+const ts = 'TS-TypeScript';
 
 console.log(happyHoliday);
 console.log(happyHewYear);
 console.log(happyDeveloper);
+console.log(ts);
